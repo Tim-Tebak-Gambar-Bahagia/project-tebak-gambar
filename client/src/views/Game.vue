ba29007cf3c290d2c3d61a41f9705a54085826a7
merchikarzk <merchika11@gmail.com>
2020-09-18 02:03:39 +0700

    template

---
+++ client/src/views/Game.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id="gamePage">
+        <div class="page-left">
+            <h1>whAtS oN yoUr scReeN</h1>
+            <div class="card mt-5" style="width: 25rem;">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Name | Score </li>
+                    <li class="list-group-item">Name | Score</li>
+                    <li class="list-group-item">Name | Score</li>
+                </ul>
+            </div>
+        </div>
+        <div class="page-right">
+            <div class="card bg-dark text-white">
+                <img src="https://images.unsplash.com/photo-1450133064473-71024230f91b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" class="card-img" alt="">
+                <div class="card-img-overlay">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="card">
+                        <h3>Jawaban</h3>
+                        <div class="card-body">
+                            <input type="text">
+                            <a href="#" class="btn btn-primary">submit</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="card">
+                        <h3>Obrolan</h3>
+                        <div class="card-body">
+                            <input type="text">
+                            <a href="#" class="btn btn-primary">submit</a>
+                        </div>
+                    </div>
+                </div>
+                </div>
+
+        </div>
+    </div>
+</template>
