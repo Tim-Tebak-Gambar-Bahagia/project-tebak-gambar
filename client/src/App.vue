4dcf76a6c3ee75cd487a7ecaf100f912c9b17585
mariosupit <armandosupit@gmail.com>
2020-09-18 00:33:45 +0700

    add leaderboard page

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/leaderboard">Leaderboard</router-link>
+    </div>
     <router-view/>
   </div>
 </template>
 
 <style>
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
