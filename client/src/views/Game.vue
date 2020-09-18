cfb3e4dfbbd3516d4b7cff30df168af0c864bb84
merchikarzk <merchika11@gmail.com>
2020-09-18 09:38:13 +0700

    minor update

--- client/src/views/Game.vue
+++ client/src/views/Game.vue
@@ -2,7 +2,7 @@
     <div id="gamePage">
         <div class="page-left">
             <h1>whAtS oN yoUr scReeN</h1>
-            <div class="card mt-5" style="width: 25rem;">
+            <div class="card mt-7" style="width: 45rem;">
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">Name | Score </li>
                     <li class="list-group-item">Name | Score</li>
@@ -11,16 +11,17 @@
             </div>
         </div>
         <div class="page-right">
-            <div class="card bg-dark text-white">
+            <div class="card">
                 <img src="https://images.unsplash.com/photo-1450133064473-71024230f91b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" class="card-img" alt="">
-                <div class="card-img-overlay">
-                </div>
             </div>
             <div class="row">
                 <div class="col-sm-6">
                     <div class="card">
                         <h3>Jawaban</h3>
                         <div class="card-body">
+                            <div class="cont">
+                                
+                            </div>
                             <input type="text">
                             <a href="#" class="btn btn-primary">submit</a>
                         </div>
@@ -30,6 +31,9 @@
                     <div class="card">
                         <h3>Obrolan</h3>
                         <div class="card-body">
+                            <div class="cont">
+
+                            </div>
                             <input type="text">
                             <a href="#" class="btn btn-primary">submit</a>
                         </div>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -29,7 +29,7 @@ export default {
   },
   methods: {
     addUser() {
-      const sound = "videoplayback_online-audio-converter.com.mp3"
+      const sound =  require(("../assets/videoplayback_online-audio-converter.com.mp3"));
       if (sound) {
         const audio = new Audio(sound);
         audio.play();
